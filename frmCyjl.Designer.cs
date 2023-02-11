fae5c21d6d519bc4ee80f9e6e55a0bc3f0c7307e
博文哥哥 <qibowen7852008@qq.com>
2023-02-11 15:34:15 +0800

    Add files via upload

---
+++ frmCyjl.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SuperWenZiToolBox
+{
+    partial class frmCyjl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbltip = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始游戏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(288, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "复位";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(49, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(75, 172);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "开接成语";
+            // 
+            // lbltip
+            // 
+            this.lbltip.AutoSize = true;
+            this.lbltip.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbltip.Location = new System.Drawing.Point(160, 241);
+            this.lbltip.Name = "lbltip";
+            this.lbltip.Size = new System.Drawing.Size(55, 16);
+            this.lbltip.TabIndex = 5;
+            this.lbltip.Text = "提示：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(195, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "分数";
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.lblscore.Location = new System.Drawing.Point(283, 138);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(16, 16);
+            this.lblscore.TabIndex = 7;
+            this.lblscore.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(197, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "时间";
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.lbltime.Location = new System.Drawing.Point(285, 189);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(25, 16);
+            this.lbltime.TabIndex = 9;
+            this.lbltime.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "预设时间";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30"});
+            this.comboBox1.Location = new System.Drawing.Point(288, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // frmCyjl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 328);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbltip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmCyjl";
+            this.Text = "成语接龙";
+            this.Load += new System.EventHandler(this.frmCyjl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbltip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ frmCyjl.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SuperWenZiToolBox
+{
+    public partial class frmCyjl : Form
+    {
+        public int settime;
+        public string oldt;
+        public frmCyjl()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCyjl_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "10";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==13)
+            {
+                button1.Focus();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbltip.Text = "";
+            settime = Convert.ToInt32(comboBox1.Text);
+            int score = Convert.ToInt32(lblscore .Text );
+            comboBox1.Enabled = false;
+            string newt = textBox1.Text;
+            if (listBox1.Items.Count ==0) 
+            {
+                lbltime.Text = settime.ToString();
+                listBox1.Items.Add(newt);
+                oldt = newt;
+                newt = "";
+                label1.Text = "请接龙";
+                textBox1.Text = "";
+                button1.Text = "接龙";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                
+
+                if(listBox1 .FindStringExact (newt )==-1)
+                {
+                    if (newt.Length!=4)
+                    {
+                        lbltip .Text ="目前仅支持四字词语";
+                    }
+                    else
+                    {
+                        if (oldt.Substring(oldt .Length -1,1) == newt.Substring(0, 1))
+                        {
+                            lbltime.Text = settime.ToString();
+                            timer1.Enabled = true;
+                            listBox1.Items.Add(newt);
+                            oldt = newt;
+                            newt = "";
+                            score = score + 1;
+                            lblscore.Text = score.ToString();
+                        }
+                        else
+                        {
+                            lbltip.Text = "请正确接龙";
+                        }
+                    }
+                }
+                else
+                {
+                    lbltip.Text = "成语已经用过了";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (lbltime .Text !="0")
+            {
+                int lasttime = Convert.ToInt32(lbltime.Text);
+                lasttime = lasttime - 1;
+                lbltime.Text = lasttime.ToString();
+            }
+            else
+            {
+                lbltip.Text = "游戏结束";
+                button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+            comboBox1.Enabled = true;
+            timer1.Enabled = false;
+            lbltime.Text = "";
+            lblscore.Text = "0";
+        }
+    }
+}
